718beb5fad55227c44afd0e574ff376a5122a316
Michael Willis <michael.willis@rackspace.com>
2014-05-07 13:53:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Raster to SVG
+
+Converts raster images to SVG
+
+## License
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clojure-svg "0.1.0-SNAPSHOT"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main raster-to-svg.core)
