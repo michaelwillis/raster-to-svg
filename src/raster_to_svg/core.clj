814e6d426a9967b1c2c0f88769344bad9e0c8419
Michael Willis <willismichael@gmail.com>
2014-05-08 07:48:00 -0500

    Average intensity per block

--- src/raster_to_svg/core.clj
+++ src/raster_to_svg/core.clj
@@ -3,24 +3,22 @@
            [java.awt.image BufferedImage])
   (:require [clojure.java.io]))
 
-(defn get-pixels-for-block [raster block-x block-y block-size]
-  (for [x (range (* block-x block-size) (* (inc block-x) block-size))
-        y (range (* block-y block-size) (* (inc block-y) block-size))]
-    (.getPixel raster x y nil)))
+(defn average [numbers]
+  (/ (apply + numbers) (count numbers)))
 
-(defn process-block [raster block-x block-y block-size]
-  (get-pixels-for-block raster block-x, block-y, block-size))
+(defn process-block [raster block-size [x y]]
+  (average (.getPixels raster x y block-size block-size nil)))
 
 (defn process-image []
   (let [block-size 8
         filename "resources/candle.png"
         imagefile (clojure.java.io/as-file filename)
         raster (.getData (. ImageIO read imagefile))
-        width-in-blocks (/ (.getWidth raster) block-size)
-        height-in-blocks (/ (.getHeight raster) block-size)           
-        block-coords (for [x (range width-in-blocks) y (range height-in-blocks)] [x y])]
-
-    (map (fn [[x y]] (process-block raster x y block-size)) block-coords)))
+        block-coords (for [x (range 0 (.getWidth raster) block-size)
+                           y (range 0 (.getHeight raster) block-size)] [x y])]
+    (->> block-coords
+         (map (partial process-block raster block-size))
+         (zipmap block-coords))))
 
 (defn -main [& args]
   (println (process-image)))
