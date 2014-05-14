7c9b517c18c860387b6d80eb829abd1ed1283475
Michael Willis <willismichael@gmail.com>
2014-05-13 21:15:10 -0500

    WIP

--- src/raster_to_svg/core.clj
+++ src/raster_to_svg/core.clj
@@ -3,11 +3,11 @@
            [java.awt.image BufferedImage])
   (:require [clojure.java.io]))
 
-(defn average [numbers]
-  (/ (apply + numbers) (count numbers)))
+(defn get-block-pixels [raster block-size [x y]]
+  (partition 4 (.getPixels raster x y block-size block-size nil)))
 
-(defn process-block [raster block-size [x y]]
-  (average (.getPixels raster x y block-size block-size nil)))
+(defn get-pixel-intensity [[r g b a]]
+  (-> (+ r g b) (* a) (/ 255 3) (float)))
 
 (defn process-image []
   (let [block-size 8
@@ -17,7 +17,7 @@
         block-coords (for [x (range 0 (.getWidth raster) block-size)
                            y (range 0 (.getHeight raster) block-size)] [x y])]
     (->> block-coords
-         (map (partial process-block raster block-size))
+         (map (partial get-block-pixels raster block-size))
          (zipmap block-coords))))
 
 (defn -main [& args]
